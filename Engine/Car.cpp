9c2b3d4834a844bd527624eec0a4717417c27227
Bartek <bartek.blazkow@o2.pl>
2018-08-08 16:30:18 +0200

    car has bumper in simple direction

--- Engine/Car.cpp
+++ Engine/Car.cpp
@@ -106,37 +106,130 @@ void Car::draw(Graphics & gfx) const
 	drawCar(gfx);
 }
 
+//TODO: 
 void Car::drawCar(Graphics & gfx) const
 {
+	int padding = 5;
+	Color paddingColor = Colors::Red;
 	if (isSimpleDirection(dir))
 	{
+		int fixedX, fixedY, dX, dY;//delta X | delta Y
 
-		gfx.drawRectDim((int)xPos, (int)yPos, width, height, c);
+		//gfx.drawRectDim((int)xPos, (int)yPos, width, height, c);
 		switch (dir)
 		{
 		case RIGHT:
-		{
+			fixedX = xPos - width / 2;
+			fixedY = yPos - height / 2;
+
+			for (int y = fixedY; y < fixedY + height; y++)
+			{
+				int x = fixedX;
+				while (x < fixedX + width - padding)
+				{
+					gfx.putPixel(x, y, c);
+					x++;
+				}
+				while (x < fixedX + width)
+				{
+					gfx.putPixel(x, y, paddingColor);
+					x++;
+				}
+
+			}
 
 			break;
-		}
 			
 		case LEFT:
+			fixedX = xPos + width / 2;
+			fixedY = yPos - height / 2;
 
+			for (int y = fixedY; y < fixedY + height; y++)
+			{
+				int x = fixedX;
+				while (x > fixedX - width + padding)
+				{
+					gfx.putPixel(x, y, c);
+					x--;
+				}
+				while (x > fixedX - width)
+				{
+					gfx.putPixel(x, y, paddingColor);
+					x--;
+				}
+
+			}
+			
 			break;
 		case DOWN:
+			fixedX = xPos - width / 2;
+			fixedY = yPos - height / 2;
+			for (int x = fixedX; x < fixedX + width; x++)
+			{
+				int y = fixedY;
+				while (y < fixedY + height - padding)
+				{
+					gfx.putPixel(x, y, c);
+					y++;
+				}
+				while (y < fixedY + height)
+				{
+					gfx.putPixel(x, y, paddingColor);
+					y++;
+				}
+			}
 
 			break;
 		case UP:
+			fixedX = xPos - width / 2;
+			fixedY = yPos + height / 2;
+
+			for (int x = fixedX; x < fixedX + width; x++)
+			{
+				int y = fixedY;
+				while (y > fixedY - height + padding)
+				{
+					gfx.putPixel(x, y, c);
+					y--;
+				}
+				while (y > fixedY - height)
+				{
+					gfx.putPixel(x, y, paddingColor);
+					y--;
+				}
+			}
+
 
 			break;
 		default:
 			break;
 		}
+
 	}
 	else
 	{
 		const int newWidth = (int)((float)width / (float)sqrt(2));
 		const int newHeight = (int)((float)height / (float)sqrt(2));
 		gfx.draw45Rect((int)xPos, (int)yPos, newWidth, newHeight, c);
+		switch (dir)
+		{
+		case UP_RIGHT:
+		{
+
+			break;
+		}
+		case DOWN_RIGHT:
+
+			break;
+
+		case DOWN_LEFT:
+
+			break;
+		case UP_LEFT:
+
+			break;
+		default:
+			break;
+		}
 	}
 }
